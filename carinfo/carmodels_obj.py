677ef02185dbea0fd1f78175a8b125e89cb75df1
sikepc <lochlanc8@gmail.com>
2026-03-10 14:31:06 -0300

    changes

--- carinfo/carmodels_obj.py
+++ carinfo/carmodels_obj.py
@@ -4,11 +4,6 @@ import discord
 
 # View Class
 class ButtonView(discord.ui.View):
-    def __init__(self):
-        super().__init__(timeout=180)  # Set timeout for the view
-
-# Button Class
-class ModelButtons():
     @discord.ui.button(label="Mazda 2", style=discord.ButtonStyle.primary)
-    async def button_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
+    async def button_callback(self, button, interaction: discord.Interaction):
         await interaction.response.send_message(f"You selected {button.label}!", ephemeral=True)
