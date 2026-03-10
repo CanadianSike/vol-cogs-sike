608905730ca41f08dcd2ff0df07c0f111dcccb85
sikepc <lochlanc8@gmail.com>
2026-03-10 15:42:32 -0300

    changes

--- carinfo/carmodels_obj.py
+++ carinfo/carmodels_obj.py
@@ -7,23 +7,11 @@ class ModelButtons(discord.ui.View):
     # This class will create buttons for car model selection
 
     # Mazda 2 Button
-    @discord.ui.button(label="Mazda 2", row=0, slot=0, style=discord.ButtonStyle.primary)
-    async def mazda2_callback(self, button, interaction: discord.Interaction):
+    @discord.ui.button(label="Mazda 2", style=discord.ButtonStyle.primary)
+    async def mazda2_callback(self, button, interaction):
         await interaction.response.send_message(f"You selected {button.label}!", ephemeral=True)
     # Mazda 3 Button
-    @discord.ui.button(label="Mazda 3", slot=1, style=discord.ButtonStyle.primary)
-    async def mazda3_callback(self, button, interaction: discord.Interaction):
+    @discord.ui.button(label="Mazda 3", style=discord.ButtonStyle.primary)
+    async def mazda3_callback(self, button, interaction):
         await interaction.response.send_message(f"You selected {button.label}!", ephemeral=True)
-    # Mazda 6 Button
-    @discord.ui.button(label="Mazda 6", row=2, style=discord.ButtonStyle.primary)
-    async def mazda6_callback(self, button, interaction: discord.Interaction):
-        await interaction.response.send_message(f"You selected {button.label}!", ephemeral=True)
-    # Mazda CX-5 Button
-    @discord.ui.button(label="Mazda CX-5", row=3, style=discord.ButtonStyle.primary)
-    async def mazda_cx5_callback(self, button, interaction: discord.Interaction):
-        await interaction.response.send_message(f"You selected {button.label}!", ephemeral=True)
-    # Mazda CX-9 Button
-    @discord.ui.button(label="Mazda CX-9", row=4, style=discord.ButtonStyle.primary)
-    async def mazda_cx9_callback(self, button, interaction: discord.Interaction):
-        await interaction.response.send_message(f"You selected {button.label}!", ephemeral=True)
-    # Mazda MX-5 Button
+ 
