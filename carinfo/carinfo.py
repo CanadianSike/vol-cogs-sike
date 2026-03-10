b2e83d4339552d52f3c890103c19a326edb9109e
sikepc <lochlanc8@gmail.com>
2026-03-10 12:12:29 -0300

    changes

--- carinfo/carinfo.py
+++ carinfo/carinfo.py
@@ -11,15 +11,12 @@ class CarInfo(commands.Cog):
     @commands.command()
     async def carinfo(self, ctx):
         """Command for users to input their car information"""
-        await ctx.send("Please select your car model:")
-        await ctx.send(view=ModelButtons())
+        await ctx.send("Please select your car model:", view=ModelButtons())
 
 class ModelButtons(discord.ui.View): #Class for storing model buttons, will be used for car_attributes.
     @discord.ui.button(label="Mazda 2", style=discord.ButtonStyle.primary)
     async def button_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
-        button.disabled=True
-        await interaction.response.edit_message(content=f"You selected {button.label}", view=self)
-
+        await interaction.response.edit_message(content=f"You selected {button.label}", view=self)  
     @discord.ui.button(label="Mazda 3", style=discord.ButtonStyle.primary)
     async def button_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
         await interaction.response.edit_message(content=f"You selected {button.label}", view=self)
@@ -46,7 +43,6 @@ class ModelButtons(discord.ui.View): #Class for storing model buttons, will be u
         await interaction.response.edit_message(content=f"You selected {button.label}", view=self)
     @discord.ui.button(label="Mazda CX-90", style=discord.ButtonStyle.primary)
     async def button_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
-        button.disabled=True
         await interaction.response.edit_message(content=f"You selected {button.label}", view=self)
 
 
