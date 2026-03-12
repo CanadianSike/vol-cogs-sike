2c6e3a9dc8144cecc225b4e0ee3385d7c84594eb
sikepc <lochlanc8@gmail.com>
2026-03-12 10:58:40 -0300

    changes

--- carinfo/carmodels_obj.py
+++ carinfo/carmodels_obj.py
@@ -8,10 +8,10 @@ class ModelButtons(discord.ui.View):
 
     # Mazda 2 Button
     @discord.ui.button(label="Mazda 2", style=discord.ButtonStyle.primary)
-    async def mazda2_callback(self, button, interaction):
+    async def mazda2_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
         await interaction.response.send_message("You selected Mazda 2!", ephemeral=True)
     # Mazda 3 Button
     @discord.ui.button(label="Mazda 3", style=discord.ButtonStyle.primary)
-    async def mazda3_callback(self, button, interaction):
+    async def mazda3_callback(self, button: discord.ui.Button, interaction: discord.Interaction):
         await interaction.response.send_message("You selected Mazda 3!", ephemeral=True)
  
