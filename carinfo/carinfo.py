0b180eb17ea5aa4c64b87ed61d80e33f78d345d7
sikepc <lochlanc8@gmail.com>
2026-03-16 11:42:01 -0300

    changes

--- carinfo/carinfo.py
+++ carinfo/carinfo.py
@@ -1,6 +1,6 @@
 from redbot.core import commands, app_commands 
 import discord
-from .carmodels_obj import MazdaModelButtons
+from .carmodels_obj import MazdaCarList, MazdaModelButtons
 from .carmodels_obj import MazdaModelList
 
 
@@ -11,14 +11,14 @@ class CarInfo(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def model(self, ctx):
+    async def car(self, ctx):
         """Command for users to input their car information"""
         await ctx.send("Please select your car model:", view=MazdaModelButtons())
 
     @commands.command()
     async def mazdalist(self, ctx):
         """Command for users to view the list of available Mazda models"""
-        await ctx.send("Here are the available Mazda models:", view=MazdaModelList())
+        await ctx.send("Here are the available Mazda models:", view=MazdaCarList())
 
 class CarAttributes():
     """Class for storing car attributes, will be used for carinfo."""
