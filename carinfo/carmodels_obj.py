ff426b5a438aa6dfc717c8fc7c74f7e57400abd4
sikepc <lochlanc8@gmail.com>
2026-03-24 13:30:25 -0300

    changes

--- carinfo/carmodels_obj.py
+++ carinfo/carmodels_obj.py
@@ -35,7 +35,8 @@ class IsTunedButtons(discord.ui.View):
 # 
     @discord.ui.button(label="Yes", style=discord.ButtonStyle.success)
     async def yes_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_modal(view=TuneRevisionInput(), ephemeral=True)
+        await interaction.response.send_message("Please input your tune revision:", ephemeral=True)
+        await interaction.response.send_modal(TuneRevisionInput(), ephemeral=True)
 
     @discord.ui.button(label="No", style=discord.ButtonStyle.danger)
     async def no_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
